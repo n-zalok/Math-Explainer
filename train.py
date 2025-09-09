f3a23dcdc38417cbb4509329f3ff1d5355e04d68
Noor Zalouk <noor-zalouk@localhost.localdomain>
2025-09-09 16:42:32 +0300

    t5-small is not enough for the task

--- train.py
+++ train.py
@@ -1,8 +1,6 @@
 from datasets import load_dataset
 from transformers import T5ForConditionalGeneration, T5TokenizerFast, DataCollatorForSeq2Seq
-import random
 from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
-import numpy as np
 import os
 
 os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
@@ -20,18 +18,15 @@ base_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
 print("Model, Tokenizer and Collator loaded")
 
 class CustomDataCollator:
-    def __init__(self, tokenizer, model, p_irrelevant=0.05, p_relevant=0.20, max_source_length=512, max_target_length=512):
+    def __init__(self, tokenizer, model, max_source_length=512, max_target_length=250):
         self.tokenizer = tokenizer
         self.model = model
         self.base_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
-        self.p_irrelevant = p_irrelevant
-        self.p_relevant = p_relevant
         self.max_source_length = max_source_length
         self.max_target_length = max_target_length
 
     def __call__(self, features):
         new_features = []
-        texts_pool = [f["text"] for f in features]  # candidate irrelevant texts
 
         for f in features:
             title = f["title"]
@@ -42,22 +37,8 @@ class CustomDataCollator:
                 sub_title = ""
             else:
                 pass
-            context_relevant = f["text"]  # the real one
-            # pick another sample’s text as irrelevant
-            if len(features) > 1:
-                context_irrelevant = random.choice(texts_pool)
-                while context_irrelevant == context_relevant:
-                    context_irrelevant = random.choice(texts_pool)
-            else:
-                context_irrelevant = ""
 
-            r = random.random()
-            if r < self.p_irrelevant:
-                input_text = f"EXPLAIN {sub_title} {title} CONTEXT {context_irrelevant}"
-            elif r < self.p_irrelevant + self.p_relevant:
-                input_text = f"EXPLAIN {sub_title} {title} CONTEXT {context_relevant}"
-            else:
-                input_text = f"EXPLAIN {sub_title} {title}"
+            input_text = f"EXPLAIN {sub_title} {title}"
 
             label_text = f["text"]
 
@@ -69,22 +50,19 @@ class CustomDataCollator:
         return self.base_collator(new_features)
 
 
-custom_collator = CustomDataCollator(tokenizer, model, p_irrelevant=0.05, p_relevant=0.20, max_source_length=512, max_target_length=340)
+custom_collator = CustomDataCollator(tokenizer, model, max_source_length=512, max_target_length=250)
 
 training_args = Seq2SeqTrainingArguments(
-    output_dir="t5_explain_runs/exp3",
+    output_dir="t5_explain_runs/exp4",
     remove_unused_columns=False,
     per_device_train_batch_size=4,          # adjust to your VRAM
     per_device_eval_batch_size=4,
     gradient_accumulation_steps=16,          # → effective batch 64
-    num_train_epochs=3,
-    eval_strategy="steps",
-    eval_steps=1000,
+    num_train_epochs=4,
+    eval_strategy="epoch",
+    save_strategy="epoch",
     logging_steps=100,
-    predict_with_generate=True,
-    generation_max_length=340,
-    generation_num_beams=4,
-    label_smoothing_factor=0.05,
+    label_smoothing_factor=0.1,
     warmup_ratio=0.1,
     learning_rate=1e-4,
     weight_decay=0.01,
